7edd7b28850675d19b236a5b03ee88b0d8764362
yingbo.tang <yingbo.tang@westberrytech.com>
2022-12-02 09:54:57 +0000

    - 12/2第一次提交（未经程序检查）

--- svd_generate/periph/readme.md
+++ svd_generate/periph/readme.md
@@ -8,3 +8,24 @@ svd 寄存器定义完成度
   rcc
   exti
   anctl
+
+未完成外设： 
+TIM 1,2,3,4(MMS寄存器不好写,SMCR没写完所有的情况，但是暂且算写完)
+RTC (写完)
+USART 1,2,3
+WWDG (写完)
+IWDG （写完）
+ADC(补齐)
+DAC 
+DMAC 1,2(写完)
+LED（写完）
+SFM(写完，但是有个USBCON的寄存器。。。)
+I2S
+I2C 1,2
+CACHE(写完)
+SPI S1,S2,M2 
+QSPI
+USB
+FMC
+ISO
+DBGMCU(写完，但不对)
---
+++ svd_generate/periph/wb_iwdg.py
@@ -0,0 +1,90 @@
+IWDG_KR = {
+    "name": "KR",
+    "displayName": "KR",
+    "description": "Key register",
+    "addressOffset": "0x00",
+    "size": "0x20",
+    "access": "read-write",
+    "resetValue": "0x00000000",
+    "fields": {
+        "field_0": {
+            "name": "KEY",
+            "description": "Key value",
+            "bitRange": "[15:0]",
+        }
+    }
+}
+
+IWDG_PR = {
+    "name": "PR",
+    "displayName": "PR",
+    "description": "Prescaler divider register",
+    "addressOffset": "0x04",
+    "size": "0x20",
+    "access": "read-write",
+    "resetValue": "0x00000000",
+    "fields": {
+        "field_0": {
+            "name": "PR",
+            "description": "Prescaler divider",
+            "bitRange": "[2:0]",
+        }
+    }
+}
+
+IWDG_RLR = {
+    "name": "RLR",
+    "displayName": "RLR",
+    "description": "Reload register",
+    "addressOffset": "0x08",
+    "size": "0x20",
+    "access": "read-write",
+    "resetValue": "0x00000000",
+    "fields": {
+        "field_0": {
+            "name": "RL",
+            "description": "Watchdog counter reload value",
+            "bitRange": "[0:0]",
+        }
+    }
+}
+
+IWDG_SR = {
+    "name": "SR",
+    "displayName": "SR",
+    "description": "Status register",
+    "addressOffset": "0x0C",
+    "size": "0x20",
+    "access": "read-write",
+    "resetValue": "0x00000000",
+    "fields": {
+        "field_0": {
+            "name": "PVU",
+            "description": "Watchdog prescaler value update",
+            "bitRange": "[0:0]",
+        },
+        "field_1": {
+            "name": "RVU",
+            "description": "Watchdog counter reload value update",
+            "bitRange": "[1:1]",
+        }
+    }
+}
+
+iwdg_define = {
+    "name": "IWDG",
+    "description": "IWDG",
+    "groupName": "IWDG",
+    "baseAddress": "0x4000BC00",
+    "addressBlock": {
+        "offset": "0x0",
+        "size": "0x400",
+        "usage": "registers",
+    },
+    "registers": {
+        "register_0": IWDG_KR,
+        "register_1": IWDG_PR,
+        "register_2": IWDG_RLR,
+        "register_3": IWDG_SR,
+    }
+}
